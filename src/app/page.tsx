5927c1b84945b81a9ab00a5019c2c8e7b3773f33
Nicholas Hazel <nicholas.hazel@gmail.com>
2023-06-16 23:23:48 -0700

    feat: basic page up with SCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import Image from 'next/image'
-import styles from './page.module.scss'
+import styles from '@app/page.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
 
       <div className={styles.description}>
-        Welcome
+        <span className={styles.logo}>Dev</span>
+        <span className={styles.logo}>Life</span>
       </div>
 
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/hero_image.svg"
-          alt="Placeholder"
-          width={600}
-          height={400}
+          src="/comp_life.png"
+          alt="Face in computer"
+          width={480}
+          height={479}
           priority
         />
       </div>
